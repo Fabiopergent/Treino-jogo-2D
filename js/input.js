aa294b0f6e42e742ca65cd2a44ebf44ae1cc1c0f
Fabiopergent <112112448+Fabiopergent@users.noreply.github.com>
2026-02-21 21:51:38 +0000

    mudancas para mobile tambem

--- js/input.js
+++ js/input.js
@@ -2,6 +2,11 @@ export class Input {
     constructor() {
         this.keys = {};
 
+        // ===== CONTROLES TOUCH =====
+        this.setupTouchControls();
+
+        
+
         window.addEventListener("keydown", (e) => {
             this.keys[e.key] = true;
         });
@@ -10,4 +15,30 @@ export class Input {
             this.keys[e.key] = false;
         });
     }
+    
+      setupTouchControls() {
+    const map = [
+        { id: "btnLeft", key: "ArrowLeft" },
+        { id: "btnRight", key: "ArrowRight" },
+        { id: "btnJump", key: "ArrowUp" },
+        { id: "btnShoot", key: "Space" }
+    ];
+
+    map.forEach(btn => {
+        const el = document.getElementById(btn.id);
+        if (!el) return;
+
+        el.addEventListener("touchstart", e => {
+            e.preventDefault();
+            this.keys[btn.key] = true;
+        });
+
+        el.addEventListener("touchend", e => {
+            e.preventDefault();
+            this.keys[btn.key] = false;
+        });
+    });
 }
+
+}
+
